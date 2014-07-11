c3f6a5df48d9b5afb4167a52cc7d8932141195b2
Wasim Thabraze <waseem.tabraze@gmail.com>
2014-07-11 22:19:41 +0530

    Update main.py
    updated code

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
+#Texts you when your blog or website goes down!
+#Working on a working prototype.
+#Send me a pull request to report bugs or to add new features.
+#Author: Wasim Thabraze
+
 import webapp2
+import json
+import urllib2
+import contextlib
 from twilio import twiml
 from twilio.rest import TwilioRestClient
+from google.appengine.api.app_identity import get_application_id
 
+uptime_key = os.environ['uptime_key']
+twilio_sid = os.environ['twilio_sid']
+twilio_from = os.environ['twilio_from']
+twilio_to = o.environ['twilio_to']
 
-
-
-def get(self):
+def SendSMS(self):
     account_sid = "AC5e69469ba8cb4c0ae0aeee5a592db605"
-    auth_token = "561ed7bcde49ee9d294e128b572d0619"
+     auth_token = "561ed7bcde49ee9d294e128b572d0619"
     client = TwilioRestClient(account_sid, auth_token)
-    rv = client.sms.messages.create(to="+919160216436",
-                                        from_="+19138714534",
-                                        body="Hello your website/blog is down!")
-        self.response.write(str(rv.sid))
+    rv = client.sms.messages.create(to=twilio_to,
+                                    from_=twilio_from,
+                                    body="Hey there! Your website is down!")
+        #self.response.write(str(rv.sid))
+
+class WebsiteStatus(webapp2.RequestHandler):
+    def get(self):
+        with contextlib.closing(urllib2.urlopen(uptime_api)) as data
+        result = json.load(data)
+
+        for m in result['monitors']['monitor']:
+            if m['status'] == '9':
+                SendSMS()
+
 
 app = webapp2.WSGIApplication([('/send_sms', SendSMS)],
                               debug=True)

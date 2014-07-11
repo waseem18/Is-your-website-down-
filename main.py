898d26a13c3773e2ba4cda432539821a1b5102d8
Wasim Thabraze <waseem.tabraze@gmail.com>
2014-07-11 22:19:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from twilio.rest import TwilioRestClient
 
 
 
-class SendSMS(webapp2.RequestHandler):
-    def get(self):
-        account_sid = "AC5e69469ba8cb4c0ae0aeee5a592db605"
-        auth_token = "561ed7bcde49ee9d294e128b572d0619"
-        client = TwilioRestClient(account_sid, auth_token)
-        rv = client.sms.messages.create(to="+919160216436",
+
+def get(self):
+    account_sid = "AC5e69469ba8cb4c0ae0aeee5a592db605"
+    auth_token = "561ed7bcde49ee9d294e128b572d0619"
+    client = TwilioRestClient(account_sid, auth_token)
+    rv = client.sms.messages.create(to="+919160216436",
                                         from_="+19138714534",
                                         body="Hello your website/blog is down!")
         self.response.write(str(rv.sid))
